a31180d170c1ca4cc138e855d1c2dccfccf12a6f
kodieswaran <131963665+KODIESWARAN@users.noreply.github.com>
2024-07-13 15:01:18 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,65 @@
+
+    const firstname = document.getElementById("first");
+    const lastname = document.getElementById("last");
+    const email = document.getElementById("email");
+    const password = document.getElementById("pass");
+    const firstnameError = document.getElementById("firstname-error");
+    const lastnameError = document.getElementById("lastname-error");
+    const emailError = document.getElementById("email-error");
+    const passwordError = document.getElementById("password-error");
+  
+    const errorIconFirstname = document.querySelector(".errorIcon-firstname");
+    const errorIconLastname = document.querySelector(".errorIcon-lastname");
+    const errorIconEmail = document.querySelector(".errorIcon-email");
+    const errorIconPassword = document.querySelector(".errorIcon-password");
+
+
+  const validateEmail = function (email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+  const handleSubmit = function (e) {
+    e.preventDefault();
+    
+   
+
+    if (firstname.value === ""){
+      firstnameError.textContent = "firstname cannot be empty";
+      errorIconFirstname.classList.remove("hidden");
+
+    } else {
+      firstnameError.textContent = " ";
+      errorIconFirstname.className.add("hidden");
+    }
+    if (lastname.value === "") {
+      lastnameError.textContent = "lastname cannot be empty";
+      errorIconLastname.classList.remove("hidden");
+      
+    } else {
+      lastnameError.textContent = " ";
+      errorIconLastname.classList.add("hidden");
+    }
+    if (!validateEmail(email.value)) {
+      emailError.textContent = "email cannot be empty";
+      errorIconEmail.classList.remove("hidden");
+     
+    } else {
+      emailError.textContent = " ";
+      errorIconEmail.classList.add("hidden");
+    }
+    if (password.value === "") {
+      passwordError.textContent = "password cannot be empty";
+      errorIconPassword.classList.remove("hidden");
+      
+    } else {
+      passwordError.textContent = " ";
+      errorIconPassword.classList.add("hidden");
+    }
+
+    
+    
+  };
+  
+  const form = document.getElementById("form");
+  form.addEventListener("submit", handleSubmit);
+  
